aab43a930ac32314af940cf257f2ee99dde7a375
Colin Gross <grosscol@gmail.com>
2017-05-17 15:00:54 -0400

    Fix naming collision with metadata and params metadata is about the payload, not just the parameters of the payload.

--- utils/kobbler.rb
+++ utils/kobbler.rb
@@ -11,26 +11,26 @@ class Kobbler
   INPUT_MESSAGE_TEMPLATE  = File.read(INPUT_MESSAGE_ERB_FILE)
   OUTPUT_MESSAGE_TEMPLATE = File.read(OUTPUT_MESSAGE_ERB_FILE)
 
-  def self.kobble(input_dir)
+  def self.kobble(input_dir, output_dir=nil)
     if input_dir.nil?
       puts "Input directory required."
       return
     end
 
-    # output to current directory
-    output_dir = Dir.new(Dir.pwd)
+    # output defaults to current directory
+    output_dir ||= Dir.new(Dir.pwd)
 
-    # get params hash
-    params = load_params(input_dir)
+    # get metadata from file in directory
+    metadata = load_metadata(input_dir)
 
     # get payload hash
     payload = load_payload(input_dir)
 
-    # construct input message based on params
-    input_message = build_template(params, INPUT_MESSAGE_TEMPLATE)
+    # construct input message based on metadata
+    input_message = build_template(metadata, INPUT_MESSAGE_TEMPLATE)
 
-    # construct output message based on params
-    output_message = build_template(params, OUTPUT_MESSAGE_TEMPLATE)
+    # construct output message based on metadata
+    output_message = build_template(metadata, OUTPUT_MESSAGE_TEMPLATE)
 
     # create knowledge object hash
     kobject = { inputMessage: input_message,
@@ -43,15 +43,15 @@ class Kobbler
     File.write( File.join(Dir.pwd, outfile_name), kobject.to_json ) 
   end
 
-  def self.build_template(params, template)
+  def self.build_template(metadata, template)
     template = Erubis::Eruby.new(template)
-    template.result(params)
+    template.result(metadata)
   end
 
-  def self.load_params(input_dir)
-    params_path = find_params_file(input_dir)
-    if params_path
-      YAML.load_file(params_path)
+  def self.load_metadata(input_dir)
+    metadata_path = find_metadata_file(input_dir)
+    if metadata_path
+      YAML.load_file(metadata_path)
     else
       {}
     end
@@ -74,9 +74,9 @@ class Kobbler
     File.join(input_dir, filename) if filename
   end
 
-  def self.find_params_file(input_dir)
+  def self.find_metadata_file(input_dir)
     filename = Dir.entries(input_dir).find do |entry|
-      File.basename(entry) == 'params.yml'
+      File.basename(entry) == 'metadata.yml'
     end
     File.join(input_dir, filename) if filename
   end
